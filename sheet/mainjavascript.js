3e8b1b3eaf39a26fa4d55cc17665538864782c73
nicolai0209 <60355930+nicolai0209@users.noreply.github.com>
2020-03-04 14:04:42 +0100

    kontakt java og kontakt css

--- sheet/mainjavascript.js
+++ sheet/mainjavascript.js
@@ -12,3 +12,31 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 5000);
 }
+
+<script>
+function _(id){ return document.getElementById(id); }
+function submitForm()
+
+{
+  alert("din besked er sendt");
+	_("mybtn").disabled = true;
+	_("status").innerHTML = 'din besked er sendt ';
+	var formdata = new FormData();
+	formdata.append( "n", _("n").value );
+	formdata.append( "e", _("e").value );
+	formdata.append( "m", _("m").value );
+	var ajax = new XMLHttpRequest();
+	ajax.open( "POST", "example_parser.php" );
+	ajax.onreadystatechange = function() {
+		if(ajax.readyState == 4 && ajax.status == 200) {
+			if(ajax.responseText == "success"){
+				_("my_form").innerHTML = '<h2>Thanks '+_("n").value+', your message has been sent.</h2>';
+			} else {
+				_("status").innerHTML = ajax.responseText;
+				_("mybtn").disabled = false;
+			}
+		}
+	}
+	ajax.send( formdata );
+}
+</script>

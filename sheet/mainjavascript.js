036ad94011d07ab6784fb212258483b62da8f1e8
deKrak <60356327+deKrak@users.noreply.github.com>
2020-03-05 00:13:10 +0100

    slideshow drillede

--- sheet/mainjavascript.js
+++ sheet/mainjavascript.js
@@ -38,22 +38,3 @@ function submitForm()
 	ajax.send( formdata );}
 
 /* slideshow forside */
-var slideIndex = 1;
-
-showimage(slideIndex);
-
-function plusIndex(n){
-    showImage(slideIndex += n);
-}
-
-function showImage(n){
-    var slide = document.getElementsByClassName("slides")
-
-    if (n > slide.length) { slideIndex = 1};
-    if (n < 1) {slideIndex = slide.length};
-
-    for (var i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";
-    };
-    slide[slideIndex-1].style.display = "block";
-}

75e9f6a673b6c3f303b9a486c6dd50372ff4280e
Bakke94 <58519183+Bakke94@users.noreply.github.com>
2020-03-04 15:17:14 +0100

    Dagens arbejde

--- sheet/mainjavascript.js
+++ sheet/mainjavascript.js
@@ -1,1 +1,13 @@
-javascript
+let galleryImages = document.querySelectorAll(".gallery-img");
+let getLatestOpenedImg;
+let windowWidth = window.innerWidth;
+
+if(galleryImages) {
+    galleryImages.forEach(function(image) {
+       image.onclick = function() {
+           let getElementCss = window.getComputedStyle(image);
+           let getfullImgUrl = getElementCss.getPropertyValue("background-image");
+           let getImgUrlPos = getFullImgUrl.split("");
+           alert(getfullImgUrl);
+    });
+}

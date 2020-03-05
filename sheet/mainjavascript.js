8a96b1642b85587d02bd179430d22faf26c58e75
deKrak <60356327+deKrak@users.noreply.github.com>
2020-03-05 11:07:13 +0100

    ud med det avascript

--- sheet/mainjavascript.js
+++ sheet/mainjavascript.js
@@ -95,24 +95,6 @@ function changeImg(changeDir) {
 
 
 }
-var myIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("slides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 5000);
-}
-           let getfullImgUrl = getElementCss.getPropertyValue("background-image");
-           let getImgUrlPos = getFullImgUrl.split("");
-           alert(getfullImgUrl);
-    };
 
 // dette er en kontakt formular til kontakt siden
 function _(id){ return document.getElementById(id); }
